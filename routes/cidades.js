54c85e4b4c1ccbf5fc1d2d86456f7c06e66d0449
Ruulul <vamjunior01@gmail.com>
2023-04-16 12:17:42 -0300

    release connection

--- routes/cidades.js
+++ routes/cidades.js
@@ -4,6 +4,7 @@ module.exports = async function (fastify, opts) {
     fastify.get('/cidades', async function (req) {
         const connection = await fastify.mysql.getConnection()
         const [cidades] = await connection.query("SELECT * FROM `cidade` where id_estado=?", req.query.estado || '1')
+        connection.release()
         return cidades
     })
 }
