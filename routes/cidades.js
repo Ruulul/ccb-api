8d778c088df9a1a7a4f479ea2847a4e2c05c8e9a
Ruulul <vamjunior01@gmail.com>
2023-04-16 11:54:20 -0300

    cidades

--- routes/cidades.js
+++ routes/cidades.js
@@ -1,7 +1,9 @@
 'use strict';
 
 module.exports = async function (fastify, opts) {
-    fastify.get('/cidades', function (request, response) {
-        response.send()
+    fastify.get('/cidades', async function (req) {
+        const connection = await fastify.mysql.getConnection()
+        const [cidades] = await connection.query("SELECT * FROM `cidade` where id_estado=?", req.query.estado || '1')
+        return cidades
     })
 }
